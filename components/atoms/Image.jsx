ffc543e37814778d5a2217dd809b805605a2c537
devmararac <dev.mararac@gmail.com>
2026-09-13 14:59:30 +0800

    Fix image paths for GitHub Pages

--- components/atoms/Image.jsx
+++ components/atoms/Image.jsx
@@ -1,13 +1,21 @@
+
 export default function Image({
   src,
   alt,
   className = "",
 }) {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
+  const imageSrc = src.startsWith("/")
+    ? `${basePath}${src}`
+    : src;
+
   return (
     <img
-      src={src}
+      src={imageSrc}
       alt={alt}
       className={`w-full object-cover ${className}`}
     />
   );
 }
+
